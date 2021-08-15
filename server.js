f65fc99edaa0cc6b62538037171b2736e60859f2
Todd Kelly <donkeyskates21@gmail.com>
2021-08-15 00:00:33 -0400

    deployed

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-//heroku
+
 mongoose.connect(
   process.env.MONGODDB_URI || "mongodb://localhost/budget",
   {
