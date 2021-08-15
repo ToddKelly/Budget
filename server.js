2d33d759696e60b0b515668ee847cefd8dcf98bd
Todd Kelly <donkeyskates21@gmail.com>
2021-08-15 02:23:40 -0400

    deploy

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 //heroku
 mongoose.connect(
-  process.env.MONGODDB_URI || "mongodb://localhost/budget",
+  process.env.MONGODB_URI || "mongodb://localhost/budget",
   {
   useNewUrlParser: true,
   useFindAndModify: false,
